651e1f40bd2064ce1a9b072e7e913d299d56722b
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2023-01-21 00:44:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function loader(element) {
         if (element.textContent === '....') {
             element.textContent = '';
         }
-    }, 300);
+    }, 3000);
 }
 
 function typeText(element, text) {
