61554069c0b2b7fd6120c3a1a9bafb9e51fa94bf
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2023-02-19 11:41:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ const handleSubmit = async (e) => {
 }
 
 form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e3) => {
+form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
         handleSubmit(e)
     }
